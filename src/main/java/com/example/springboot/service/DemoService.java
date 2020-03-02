a9db1cb3055b60c3a7b9e981e64a29ff61706b78
thrillerzw <295380007@qq.com>
2020-03-02 10:46:47 +0800

    test Transactional

--- src/main/java/com/example/springboot/service/DemoService.java
+++ src/main/java/com/example/springboot/service/DemoService.java
@@ -1,5 +1,6 @@
 package com.example.springboot.service;
 
 public interface DemoService {
+    public void transactionalService();
     void print();
 }
--- src/main/java/com/example/springboot/service/impl/DemoServiceImpl.java
+++ src/main/java/com/example/springboot/service/impl/DemoServiceImpl.java
@@ -1,10 +1,34 @@
 package com.example.springboot.service.impl;
 
+import com.example.springboot.mappers.ExampleMapper;
+import com.example.springboot.mappers.StudentMapper;
+import com.example.springboot.pojo.Example;
 import com.example.springboot.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class DemoServiceImpl implements DemoService {
+
+    @Autowired
+    StudentMapper studentMapper;
+    @Autowired
+    ExampleMapper exampleMapper;
+    @Transactional
+    public void transactionalService(){
+        Example example=new Example();
+        example.setId(1L);
+        example.setStatus((short)100);
+        exampleMapper.updateByPrimaryKeySelective(example);
+//        int a=10/0;
+        Example example2=new Example();
+        example2.setId(2L);
+        example2.setStatus((short)200);
+        exampleMapper.updateByPrimaryKeySelective(example2);
+
+
+    }
 
     @Override
     public void print() {
