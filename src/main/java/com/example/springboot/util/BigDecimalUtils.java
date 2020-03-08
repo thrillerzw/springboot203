1e4e1e245664cdc952c65054b94de9c761142b53
thrillerzw <295380007@qq.com>
2020-03-08 19:32:48 +0800

    update bigdecimal

--- src/main/java/com/example/springboot/util/BigDecimalUtils.java
+++ src/main/java/com/example/springboot/util/BigDecimalUtils.java
@@ -6,43 +6,56 @@ import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
 public class BigDecimalUtils {
-	//double加法
-	public static double add(double d1, double d2) {
+	//double加法,不要用 new BigDecimal(d1); 使用这种方式BigDecimal.valueOf(d1);
+	public static BigDecimal add(double d1, double d2) {
 		BigDecimal b1 = new BigDecimal(d1);
 		BigDecimal b2 = new BigDecimal(d2);
-		return b1.add(b2).doubleValue();
+		//b1.add(b2).doubleValue();
+		return b1.add(b2);
+	}
+	//double加法2
+	public static BigDecimal add2(double d1, double d2) {
+		BigDecimal b1 = BigDecimal.valueOf(d1);
+		BigDecimal b2 = BigDecimal.valueOf(d2);
+		return b1.add(b2);
+	}
+	//double加法3
+	public static BigDecimal addByString(String d1, String d2) {
+		BigDecimal b1 = new BigDecimal(d1);
+		BigDecimal b2 = new BigDecimal(d2);
+		return b1.add(b2);
 	}
 	//double减法
-	public static double sub(double d1, double d2) {
+	public static BigDecimal sub(double d1, double d2) {
 		BigDecimal b1 = new BigDecimal(d1);
 		BigDecimal b2 = new BigDecimal(d2);
-		return b1.subtract(b2).doubleValue();
+		return b1.subtract(b2);
 	}
 	//double乘法
-	public static double mul(double d1, double d2) {
+	public static BigDecimal mul(double d1, double d2) {
 		BigDecimal b1 = new BigDecimal(d1);
 		BigDecimal b2 = new BigDecimal(d2);
-		return b1.multiply(b2).doubleValue();
+		return b1.multiply(b2);
 	}
     /**
      * double除法运算，四舍五入后保留小数点指定位数
      * @param d1 除数
      * @param d2 被除数
-     * @param len 小数点后保留几位
+     * @param scale 小数点后保留几位
      * @return
      */
-	public static double div(double d1, double d2, int len) {
+	public static BigDecimal div(double d1, double d2, int scale) {
 		BigDecimal b1 = new BigDecimal(d1);
 		BigDecimal b2 = new BigDecimal(d2);
-		return b1.divide(b2, len, BigDecimal.ROUND_HALF_UP).doubleValue();
+		return b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP);
 	}
 	//double四舍五入操作
-	public static double round(double d, int len) {
+	public static BigDecimal round(double d, int len) {
 		BigDecimal b1 = new BigDecimal(d);
 		BigDecimal b2 = new BigDecimal(1);
 		// 任何一个数字除以1都是原数字
 		// ROUND_HALF_UP表示四舍五入的操作。RoundingMode.HALF_UP包装了BigDecimal.ROUND_HALF_UP，一样的
-		return b1.divide(b2, len, BigDecimal.ROUND_HALF_UP).doubleValue();
+		return b1.divide(b2, len, BigDecimal.ROUND_HALF_UP);
 	}
 	//BigDecimal比较大小,不要用equals
 	public static int compare(BigDecimal d1 ,BigDecimal d2){
@@ -86,6 +99,8 @@ public class BigDecimalUtils {
 	public static void main(String[] args) {
 		  	double a=10002222.1254567;
 		  	double b=10.005;
+			String d="10002222.1254567";
+			String e="10.005";
 			System.out.println("a>b="+(a>b));
 			System.out.println(round(a,2));
 			System.out.println(round(b,2));
@@ -97,5 +112,10 @@ public class BigDecimalUtils {
 			BigInteger bi=new BigInteger("9223372036854775808");
 			BigInteger bi2=new BigInteger("2");
 			System.out.println(bi.add(bi2));
+
+			//加法
+			System.out.println(add(a,b)); //10002232.1304567001014955707205444923602044582366943359375
+			System.out.println(add2(a,b)); //10002232.1304567
+			System.out.println(addByString(d,e));//10002232.1304567
 	}
 }
